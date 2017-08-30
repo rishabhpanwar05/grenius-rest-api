90dc2c01cf4750a9965d49526610319c3d101739
suryansh.soni <soni.suryansh11130@gmail.com>
2017-08-30 18:43:19 +0530

    production server changed config

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports={
 	version: '0.0.1',
 	env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3001,
-    base_url: process.env.BASE_URL || 'http://localhost:3000',
+    base_url: process.env.BASE_URL || 'http://localhost:3001',
     db: {
         uri: 'mongodb://127.0.0.1:27017/greniusTest',
     },
