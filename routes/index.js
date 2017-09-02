242282fa19c6a9fd4fd6bc0bb73769835831cc5e
suryansh.soni <soni.suryansh11130@gmail.com>
2017-09-02 19:36:46 +0530

    mail

--- routes/index.js
+++ routes/index.js
@@ -1370,8 +1370,8 @@ server.post('/generatePasscode',function(req,res,next){
 				to: req.body.emailId, // list of receivers
 				subject: "Wordly Account Password Reset", // Subject line
 				text: "", // plaintext body
-				html: "Hi <b>"+ user.name +"</b>,<br>You recently requested to reset your password for your Wordly account. In your app, key in the passkey below to reset your password. This password reset is only valid for the next hour.<br><b>"+
-						passcode+"</b><br>If you did not request a password reset, please ignore this email or contact support if you have questions.<br>Thanks,<br><b>Team Wordly</b>" // html body
+				html: "Hi <b><h3>"+ user.name +"</h3></b>,<br>You recently requested to reset your password for your Wordly account. In your app, key in the passkey below to reset your password. This password reset is only valid for the next hour.<br><b>"+
+						passcode+"</b><br>If you did not request a password reset, please ignore this email or contact support if you have questions.<br>Thanks,<br><b><h4>Team Wordly</h4></b>" // html body
 			}; 
 			transport.sendMail(mailOptions, function(error, info){
 				if(error){
@@ -1437,7 +1437,7 @@ server.post('/updatePassword',function(req,res,next){
 			var status = user.verifyPasscode(req.body.passcode);
 			if(status==true){
 				user.setPassword(req.body.password);
-				res.send(200,{"message":"Passcode Updated successfully","status":true});
+				res.send(200,{"message":"Password Updated successfully","status":true});
 			}else{
 				res.send(200,{"message":"Wrong Passcode","status":false});
 			}
