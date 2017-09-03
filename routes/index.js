dd6928cd3e827338e05875ead22a46d5007dcdbe
suryansh.soni <soni.suryansh11130@gmail.com>
2017-09-03 12:25:12 +0530

    CheckBM

--- routes/index.js
+++ routes/index.js
@@ -1329,7 +1329,7 @@ server.post('/addBookmark',function(req,res,next){
 
 
 server.post('/bookmarks',function(req,res,next){
-	console.log("Sending Bookmarks")
+	/*console.log("Sending Bookmarks")
 	req.body=qs.parse(req.body);
 	console.log(req.body)
 	Bookmark.findOne({userId:req.body.userId},function(err,bookmark){
@@ -1348,6 +1348,23 @@ server.post('/bookmarks',function(req,res,next){
 				words=[];
 				res.send(200,words);
 			}
+			next()
+		})
+		
+		*/
+		
+		console.log("Sending Bookmarks")
+	console.log(req.body)
+	req.body=qs.parse(req.body);
+	Bookmark.findOne({userId:req.body.userId},function(err,bookmark){
+			if (err) {
+				res.send(404,{"message":err,"status":false});
+				next()
+			}
+			console.log(bookmark)
+			var words=bookmark.words
+			console.log(words)
+			res.send(words);
 			next()
 		})
 })
