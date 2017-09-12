ed6656c8241e2d9067ed18d0ae829fd7627d3f48
suryansh.soni <soni.suryansh11130@gmail.com>
2017-09-12 20:34:50 +0530

    updated server details

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 
 module.exports={
 	name: 'GREnius',
-	version: '0.0.1',
+	version: '0.0.2',
 	env: process.env.NODE_ENV || 'development',
-    port: process.env.PORT || 3001,
-    base_url: process.env.BASE_URL || 'http://localhost:3001',
+    port: process.env.PORT || 3000,
+    base_url: process.env.BASE_URL || 'http://localhost:3000',
     db: {
-        uri: 'mongodb://127.0.0.1:27017/greniusTest',
+        uri: 'mongodb://127.0.0.1:27017/grenius',
     },
 	MY_SECRET:"thisismysecretcodekjkbku2441gvbjnjsnf1412321jnwjenvajdbgdh2fjwbghbfkj62ngiu",
     MY_GOOGLE_API_KEY:"AIzaSyDlDHuANynM95U0K2IKnsqF3o7pTFrzP3A",
